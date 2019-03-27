f0de8e85fb0f0d0ab17dccd5518687437cab7c11
SKORKA Christopher <Christopher.Skorka@dnrme.qld.gov.au>
2019-03-27 16:59:20 +1000

    segy reader

--- document-metadata-extraction/SEG Reader/segy-reader.py
+++ document-metadata-extraction/SEG Reader/segy-reader.py
@@ -1,12 +1,27 @@
 import segpy
 import numpy
+import sys
 
 from segpy.reader import create_reader
 from segpy.writer import write_segy
 
 print("a")
 
-with open("2.sgy", 'rb') as segy_in_file:
+with open(sys.argv[1], 'rb') as segy_in_file:
     # The seg_y_dataset is a lazy-reader, so keep the file open throughout.
     seg_y_dataset = create_reader(segy_in_file)  # Non-standard Rev 1 little-endian
-    print(seg_y_dataset.num_traces())
+
+    print(seg_y_dataset.__dict__)
+
+    properties = ['num_traces', 'dimensionality', 'data_sample_format', 'data_sample_format_description', '_revision', '_bytes_per_sample', '_max_num_trace_samples']
+
+    for p in properties: 
+        if hasattr(seg_y_dataset, p):
+            a = getattr(seg_y_dataset, p)
+            print(p, a() if callable(a) else a)
+        else:
+            print(p, 'is not an attribute')
+
+    print(seg_y_dataset.trace_header(0))
+
+    #print('', seg_y_dataset.())
