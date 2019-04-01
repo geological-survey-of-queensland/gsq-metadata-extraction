53d98f39b804144720b72d4bde40f34917dc13b2
SKORKA Christopher <Christopher.Skorka@dnrme.qld.gov.au>
2019-04-01 14:49:10 +1000

    processor encodes everything and stores it

--- filepath-metadata-extraction/preprocessing.py
+++ filepath-metadata-extraction/preprocessing.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import pickle
+import math
 import re
 
 # tokens used to communicate non character entities
@@ -6,23 +9,29 @@ tokens = ['<Padding>', '<Go>', '<EndOfString>', '<UnknownChar>', '<SurveyNum>', 
 
 # get set of characters to be used, use static preset list of characters
 # TODO use all characters from entire training set?
-available_chars = list(" ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_().,\\/\"':&")
+available_chars = list(" ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_().,\\/\"':&")
 
 # generate character to int and int to character maps
 char_to_int = {c: i for i, c in enumerate(tokens + available_chars)}
 int_to_char = {i: c for c, i in char_to_int.items()}
 
-def process_training_data():
-
-    raw_source_file = 'SHUP 2D Files Training Data.csv'
-    modified_source_file = 'Processed 2D Files Training Data.csv'
 
 
-    # 
+# main program to perform all of preprocessing
+def main():
+    data = process_training_data('SHUP 2D Files Training Data.csv')
+    encoded_data = encode_training_data(data)
+
+    encoded_data.to_csv('training_data.csv', index=False)
+    pickle.dump((encoded_data), open('training_data.p', 'wb'))
+
+
+
+# clean up data to remove and change some key sapects
+def process_training_data(raw_source_file):
+
     # read raw training data
-    # 
-
-    data = pd.read_csv(raw_source_file)
+    data = pd.read_csv(raw_source_file, dtype=str)
     print("Columns", data.columns.values)
 
 
@@ -45,19 +54,52 @@ def process_training_data():
     data.drop('FileName', 1, inplace=True)
     data.drop('Original_FileName', 1, inplace=True)
 
-    # save and show final structure
-    csv = data.to_csv(index=False).upper()
-    data.to_csv(modified_source_file, index=False)
-    print("Final structure")
+    # show final structure
+    print('Final structure')
     print(data.head())
+
+    return data
+
+
+
+# encode all data using global encoding dictionaries/lookup tables
+def encode_training_data(data):
+    encoded_data = pd.DataFrame()
+
+    # take each column from old data and create a new column with content where each string entry is encoded
+    for column in data.columns.values:
+        encoded = [encode(s) if type(s)==str else [] for s in data[column]]
+        encoded_data[column] = pd.Series(encoded)
+    
+    print('encoded structure')
+    print(encoded_data.head())
+
+    return encoded_data
+
 
 
 # convert a string in to a list of ints
 def encode(string):
-    return [char_to_int[char] for char in string]
+    try:
+        return [char_to_int[char] for char in string]
+    except:
+        print('error:', string, type(string))
+        return []
+
+
 
 # convert a list if ints into a string
 def decode(vector):
     ''.join(int_to_char[i] for i in vector)
 
-# if __name__ == "__main__": process_training_data()
+
+
+# load a processed pickle file
+def load(filename):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
+
+    return None
+
+
+if __name__ == "__main__": main()
