121579ea1e48f325598f0f952d01f905b3d6e6cb
SKORKA Christopher <Christopher.Skorka@dnrme.qld.gov.au>
2019-04-03 16:23:43 +1000

    creates padded vectorized dataset

--- filepath-metadata-extraction/preprocessing.py
+++ filepath-metadata-extraction/preprocessing.py
@@ -10,11 +10,11 @@ tokens = ['<Padding>', '<Go>', '<EndOfString>', '<UnknownChar>', '<SurveyNum>', 
 # get set of characters to be used, use static preset list of characters
 # TODO use all characters from entire training set?
 available_chars = list(" ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_().,\\/\"':&")
-char_count = len(available_chars)
 
 # generate character to int and int to character maps
 char_to_int = {c: i for i, c in enumerate(tokens + available_chars)}
 int_to_char = {i: c for c, i in char_to_int.items()}
+char_count = len(char_to_int)
 
 
 
@@ -22,11 +22,10 @@ def main():
     """main program to perform all of preprocessing"""
 
     data = process_training_data('SHUP 2D Files Training Data.csv')
-    encoded_data = encode_training_data(data)
-    padded_data = {c: convert_to_unitform_matrix(encoded_data[]) for c in encoded_data.columns.values}
+    vectorized_data = vectorize_training_data(data)
+    padded_data = pad_vector_data(vectorized_data, char_to_int['<Padding>'])
 
-    encoded_data.to_csv('training_data.csv', index=False)
-    pickle.dump((encoded_data), open('training_data.p', 'wb'))
+    pickle.dump((padded_data), open('training_data.p', 'wb'))
 
 
 
@@ -65,25 +64,21 @@ def process_training_data(raw_source_file):
 
 
 
-def encode_training_data(data):
-    """encode all data using global encoding dictionaries/lookup tables"""
+def vectorize_training_data(data):
+    """vectorize all data using global encoding dictionaries/lookup tables and store variable length vectors"""
 
-    encoded_data = pd.DataFrame()
+    vectorized_data = {}
 
     # take each column from old data and create a new column with content where each string entry is encoded
-    for column in data.columns.values:
-        encoded = [encode(s) if type(s)==str else [] for s in data[column]]
-        encoded_data[column] = pd.Series(encoded)
-    
-    print('encoded structure')
-    print(encoded_data.head())
+    for feature in data.columns.values:
+        vectorized_data[feature] = [vectorize_string(s) if type(s)==str else [] for s in data[feature]]
 
-    return encoded_data
+    return vectorized_data
 
 
 
-def encode(string):
-    """convert a string in to a list of ints"""
+def vectorize_string(string):
+    """convert a string in to a vector"""
 
     try:
         return [char_to_int[char] for char in string]
@@ -94,7 +89,7 @@ def encode(string):
 
 
 def decode(vector):
-    """convert a list if ints into a string"""
+    """convert a vector into a string"""
 
     ''.join(int_to_char[i] for i in vector)
 
@@ -110,39 +105,50 @@ def load(filename):
 
 
 
-def train_test_split(data, test_frac=0.2, shuffle_before=False, shuffle_after=False):
+def train_test_split(*data, test_frac=0.2, shuffle_before=False, shuffle_after=False):
     """split dataset into train and test and optionally shuffle"""
 
-    if shuffle_before:
-        data = data.sample(frac=1)
+    middle = int(len(data[0]) * test_frac)
 
-    test = data.head(int(len(data)*test_frac))
-    train = data.tail(int(len(data)*(1-test_frac)))
+    if shuffle_before:
+        order = np.arange(len(data[0]))
+        np.random.shuffle(order)
+        data = [d[order] for d in data]
+
+    test = [d[:middle] for d in data]
+    train = [d[middle:] for d in data]
 
     if shuffle_after:
-        test = test.sample(frac=1)
-        train = train.sample(frac=1)
+        test_order = np.arange(len(test[0]))
+        np.random.shuffle(test_order)
+        test = [d[test_order] for d in test]
+        train_order = np.arange(len(train[0]))
+        np.random.shuffle(train_order)
+        train = [d[train_order] for d in train]
 
     return train, test
         
 
-def convert_to_unitform_matrix(data, pad_token):
-    """for each column create a matrix wide enough to fit all samples and fill with teh padding token"""
+def pad_vector_data(data, pad_token):
+    """for each feature create a matrix wide enough to fit all samples and fill with the padding token"""
 
-    height = len(data)
-    width = max([len(l) for l in data[column]])
+    padded_data = {}
 
-    padded_data = np.full((height, width), pad_token, np.int32)
+    for feature in data:
+        vector_list = data[feature]
 
-    for i in len(data):
-        row = data.loc[i]
-        length = len(row)
-        padded_data[:length] = row
+        height = len(vector_list)
+        width = max([len(v) for v in vector_list])
+
+        matrix = np.full((height, width), pad_token, np.int32)
+
+        for i, v in enumerate(vector_list):
+            matrix[i, :len(v)] = v
+
+        padded_data[feature] = matrix
 
     return padded_data
         
-
-
 
 
 if __name__ == "__main__": main()
